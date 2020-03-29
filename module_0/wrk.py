a6937fae360d27415c420c37c650d5f8f9e91fac
Boriskickdick <boris1488hh@gmail.com>
2020-03-30 01:08:48 +0300

    Changed Predict depending on how much it more or less than Number

--- module_0/wrk.py
+++ module_0/wrk.py
@@ -1,28 +1,48 @@
 import numpy as np
+
+
 def game_core_v2(number):
     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
        Функция принимает загаданное число и возвращает число попыток'''
     count = 0
-    predict = np.random.randint(1,100)
+    predict = np.random.randint(2, 100)
     while number != predict:
-        count+=1
-        if number > predict: 
+        count += 1
+        if number > predict + 25:
+            predict += 25
+        elif number > predict + 20:
+            predict += 20
+        elif number > predict + 10:
+            predict += 10
+        elif number > predict + 5:
             predict += 5
-        elif number < predict: 
-            predict -= 4
-    return(count) # выход из цикла, если угадали
-        
-        
+        elif number > predict:
+            predict += 1
+        elif number + 25 < predict:
+            predict -= 25
+        elif number + 20 < predict:
+            predict -= 20
+        elif number + 10 < predict:
+            predict -= 10
+        elif number + 5 < predict:
+            predict -= 5
+        elif number < predict:
+            predict -= 1
+
+    return count  # выход из цикла, если угадали
+
+
 def score_game(game_core_v2):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1, 101, size=(1000))
+    random_array = np.random.randint(1, 101, size=1000)
     for number in random_array:
         count_ls.append(game_core_v2(number))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    return(score)
+    return score
+
 
 # запускаем
 score_game(game_core_v2)
