f5260e721f069153972430fa5ba14b729f52627a
Boriskickdick <boris1488hh@gmail.com>
2020-03-30 01:13:14 +0300

    Сorrected mistake in np.random.randint from 2 to 1

--- module_0/wrk.py
+++ module_0/wrk.py
@@ -5,7 +5,7 @@ def game_core_v2(number):
     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
        Функция принимает загаданное число и возвращает число попыток'''
     count = 0
-    predict = np.random.randint(2, 100)
+    predict = np.random.randint(1, 100)
     while number != predict:
         count += 1
         if number > predict + 25:
